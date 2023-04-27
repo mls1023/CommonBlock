3879bd27eef7c939cf39da32d09e07854a6f8bc4
mls1023 <mls1023@nyu.edu>
2023-04-26 21:20:14 -0400

    apr 26 changes

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, IntegerField, SelectField, SubmitField, TextAreaField, HiddenField
+from wtforms import StringField, PasswordField, IntegerField, SelectField, SubmitField, TextAreaField, HiddenField, FloatField, FileField, DecimalField
 from wtforms.validators import DataRequired, Email, Length, InputRequired, NumberRange
 
 class SearchForm(FlaskForm):
@@ -29,8 +29,20 @@ class AccountInfoForm(FlaskForm):
     bedtime = SelectField('Bed Time', choice=[('9pm'),('10pm'),('11pm'),('12am'),('1am'),('2am')])
     edit_account = SubmitField('Edit Account')
 
-class ReviewForm(FlaskForm):
-    review_type = SelectField('Review Type', choices=[('user', 'User'), ('apartment', 'Apartment')], validators=[InputRequired()])
-    name_or_address = TextAreaField('Name or Address', validators=[InputRequired()])
+class ApartmentReviewForm(FlaskForm):
     rating = IntegerField('Rating', validators=[InputRequired(), NumberRange(min=1, max=5)])
     comment = TextAreaField('Comment')
+    apartment_id = SelectField('Apartment', choices=[], coerce=int)
+
+class UserReviewForm(FlaskForm):
+    username = TextAreaField('Username', validators=[InputRequired()])
+    rating = IntegerField('Rating', validators=[InputRequired(), NumberRange(min=1, max=5)])
+    comment = TextAreaField('Comment')
+
+class StoreForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=100)])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    condition = SelectField('Condition', choices=[('new', 'New'), ('used', 'Used')], validators=[DataRequired()])
+    price = FloatField('Price', validators=[DataRequired()])
+    image = FileField('Image', validators=[DataRequired()])
+    submit = SubmitField('Post')    
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from flask import render_template, flash, redirect, url_for, request
-from flask_login import login_required, current_user, login_user, logout_user
+from flask_login import login_required, login_user, logout_user, current_user   
 from sqlalchemy import func
 from app import app, db
-from app.forms import ReviewForm, LoginForm, SignupForm, SearchForm
-from app.models import User, Apartment, Review
+from app.forms import ApartmentReviewForm, UserReviewForm, LoginForm, SignupForm, SearchForm, StoreForm
+from app.models import Furniture, User, Apartment, Review
 from geopy.distance import geodesic
 
 @app.route('/')
@@ -70,16 +70,10 @@ def search_results():
     num_bedrooms = request.args.get('num_bedrooms', type=int)
     lat = request.args.get('lat', type=float)
     lng = request.args.get('lng', type=float)
-
-    listings = Apartment.query.filter(
-    Apartment.rent >= min_rent,
-    Apartment.rent <= max_rent,
-    Apartment.num_bedrooms == num_bedrooms
-    ).all()
-
+    listings = Apartment.query.filter(Apartment.rent >= min_rent, Apartment.rent <= max_rent,
+    Apartment.num_bedrooms == num_bedrooms).all()
     filtered_listings = [listing for listing in listings if
         geodesic((lat,lng), (listing.lat, listing.lng)).miles <= 1]
-
     reviews = []
     for listing in filtered_listings:
         avg_rating = db.session.query(func.avg(Review.rating)).filter_by(apartment_id=listing.id).scalar()
@@ -92,42 +86,60 @@ def search_results():
 def edit_account():
     return render_template('edit_account.html')
 
-@app.route('/review', methods=['GET', 'POST'])
+@app.route('/review/user', methods=['GET', 'POST'])
 @login_required
-def review():
-    form = ReviewForm()
-    review_type = None
-    search_result = None
-
+def user_review():
+    form = UserReviewForm()
     if form.validate_on_submit():
-        review = Review(
-            rating=form.rating.data,
-            text=form.text.data,
-            user_id=current_user.id,
-            apartment_id=form.apartment_id.data,
-        )
-        db.session.add(review)
-        db.session.commit()
-        flash('Review has been submitted successfully.', 'success')
-        return redirect(url_for('home'))
-
-    if request.method == 'POST':
-        review_type = request.form.get('review_type')
-        query = request.form.get('query').strip()
-        
-        if review_type == 'user':
-            search_result = User.query.filter_by(username=query).first()
-        elif review_type == 'apartment':
-            search_result = Apartment.query.filter_by(address=query).first()
-            
-        if not search_result:
-            flash('No {} found with the given name/address.'.format(review_type), 'danger')
+        username = form.username.data.strip()
+        rating = form.rating.data
+        comment = form.comment.data
+        user = User.query.filter_by(username=username).first()
+        if user:
+            review = Review(rating=rating,text=comment,user_id=user.id)
+            db.session.add(review)
+            db.session.commit()
+            flash('Review has been submitted successfully.', 'success')
+            return redirect(url_for('home'))
         else:
-            form.apartment_id.choices = [(search_result.id, search_result.address)]
-            
-    return render_template('review.html', form=form, review_type=review_type, search_result=search_result)
+            flash('User with the given username does not exist.', 'danger')
+    return render_template('user_review.html', form=form)
+    
+@app.route('/review/apartment', methods=['GET', 'POST'])
+def apartment_review():
+    form = ApartmentReviewForm()
+    form.apartment_id.choices = [(a.id, a.address) for a in Apartment.query.all()]
+    if form.validate_on_submit():
+        review = Review(rating=form.rating.data, comment=form.comment.data)
+        apartment = Apartment.query.get(form.apartment_id.data)
+        apartment.reviews.append(review)
+        db.session.add(apartment)
+        db.session.commit()
+        flash('Thank you for your review!')
+        return redirect(url_for('index'))
+    else:
+        flash('Incorrect input.', 'danger')
+    return render_template('apartment_review.html', form=form)
 
 @app.route('/join_group', methods=['GET', 'POST'])
 @login_required
 def join_group():
     return render_template('join_group.html')
+
+@app.route('/store', methods=['GET'])
+def store():
+    # Handle GET request to display items and search form
+    return render_template('store.html')
+
+@app.route('/store/post', methods=['GET', 'POST'])
+@login_required
+def post_item():
+    # Handle GET and POST request to post a new item
+    form = StoreForm()
+    if form.validate_on_submit():
+        picture_file = 0 #edit
+        new_item = Furniture(picture=picture_file, condition=form.condition.data, description=form.description.data, price=form.price.data, seller=current_user)
+        db.session.add(new_item)
+        db.session.commit()
+        flash('Your item has been posted!', 'success')
+    return render_template('store_post.html',  form=form)
